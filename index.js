9b9e0dcfab444f409be63d8a8e55221db685802c
leqvin <levin@oregonstate.edu>
2022-02-04 13:00:58 -0800

    minor clean up

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/*(function() {
-  'use strict';
-  let stopbutton = document.getElementById('stopButton')
-  console.log(stpbtn)
-})();*/
-
 //Global Variables
 let stopBtn = document.getElementById('stopButton')
 let stopLt = document.getElementById('stopLight')
@@ -35,27 +29,24 @@ function goLight(){
 }
 
 function toggleLight(buttonName, lightName, className) {
-  //let buttonStatus = false
   buttonName.addEventListener("click", function() {
-    //buttonStatus = true
     if (lightName.classList.toggle(className)){
       console.log(`${buttonName.textContent} bulb on`)
     } else {
       console.log(`${buttonName.textContent} bulb off`)
     }
-    //lightName.classList.toggle(className);
     }) 
 }
 
 function mouseEnter(buttonName) {
   buttonName.addEventListener('mouseenter', e => {
-    console.log(`"Entered" ${buttonName.textContent} button`)
+    console.log(`Entered ${buttonName.textContent} button`)
   })
 }
 
 function mouseLeave(buttonName){
   buttonName.addEventListener('mouseleave', e => {
-    console.log(`"Leave" ${buttonName.textContent} button`)
+    console.log(`Leave ${buttonName.textContent} button`)
   })
 }
 
